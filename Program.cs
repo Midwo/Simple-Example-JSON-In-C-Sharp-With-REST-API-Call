924f525a31878282c6b09741d751dfed8dbb5272
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-19 15:55:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Example_JSON_In_C__With_REST_API_Call
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
