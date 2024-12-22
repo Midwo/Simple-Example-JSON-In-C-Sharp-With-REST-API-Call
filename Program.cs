30ce5c1b40ce8cfc11a665475c12c11f8bf7daee
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-22 22:14:30 +0100

    Add:  A list with deserialized data. Data display.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -6,6 +7,7 @@ using System.Net;
 using System.Net.Cache;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.X86;
+using System.Runtime.Serialization;
 using static System.Net.WebRequestMethods;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -22,91 +24,150 @@ namespace Simple_Example_JSON_In_C__With_REST_API_Call
             int idUser;
             int actualPageUser = 1;
             bool tempBoolCorrectIntValue = true;
+
+            int totalPages = 0;
+            int totalEntries = 0;
             do
             {
-                Console.Write("Patient ID: ");
-                var tempStringReadLine = Console.ReadLine();
-                tempBoolCorrectIntValue = int.TryParse(tempStringReadLine, out idUser);
-                if (!tempBoolCorrectIntValue)
-                {
-                    Console.Clear();
-                    Console.WriteLine("You have entered an incorrect Patient ID");
-                }
-            } while (!tempBoolCorrectIntValue);
-            Console.Clear();
-
-            ////Selection of data retrieval method, newer HttpClient and old, not recommended WebClient
-            ////WebClient() call with https link. Changing<user Id> to the idUser variable, and<page> to the pageUser variable.
-            //// var stringWebsiteJson = new WebClient().DownloadString("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
-            var stringWebsiteJson = await Client.GetStringAsync("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
-
-            ////Add to project "Newtonsoft.Json"  How? Project -> Manage NuGet Packages -> Newtonsoft.Json
-            ////Program.Root x = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson);
-            var deserializeJson = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-         
-
-            if (deserializeJson != null)
-            {
-                //Variables needed to define the number of pages of patient data and the number of entries.
-                //Assigning the appropriate data to the variable in the next stage of the code
-                int totalPages = deserializeJson.total_pages;
-                int totalEntries = deserializeJson.total;
-
-                //DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                //dateTime = dateTime.AddSeconds(Math.Round(1568550058964 / 1000d)).ToLocalTime();
-                //Console.WriteLine(dateTime);
-                List<long> listTimeStamp = new List<long>();
+                Console.Clear();
                 do
                 {
-                    if (actualPageUser > 1)
+                    Console.Write("Patient ID: ");
+                    var tempStringReadLine = Console.ReadLine();
+                    tempBoolCorrectIntValue = int.TryParse(tempStringReadLine, out idUser);
+                    if (!tempBoolCorrectIntValue)
                     {
-                        ////Select the new HttpClient method or the old WebClient method
-                        //stringWebsiteJson = new WebClient().DownloadString("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
-                        stringWebsiteJson = await Client.GetStringAsync("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
-
-                        deserializeJson = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson, new JsonSerializerSettings
-                        {
-                            NullValueHandling = NullValueHandling.Ignore
-                        });
+                        Console.Clear();
+                        Console.WriteLine("You have entered an incorrect Patient ID");
                     }
-                    //else if (actualPageUser == 1)
-                    //{
-                    //    Console.WriteLine(deserializeJson.data[0].timestamp);
-                    //}
-                    //if (actualPageUser == totalPages)
-                    //{
-                    //    Console.WriteLine(deserializeJson.data[deserializeJson.data.Count()-1].timestamp);
-                    //}
+                } while (!tempBoolCorrectIntValue);
+                Console.Clear();
 
-                    if (deserializeJson != null)
+                ////Selection of data retrieval method, newer HttpClient and old, not recommended WebClient
+                ////WebClient() call with https link. Changing<user Id> to the idUser variable, and<page> to the pageUser variable.
+                //// var stringWebsiteJson = new WebClient().DownloadString("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
+                var stringWebsiteJson = await Client.GetStringAsync("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
+
+                ////Add to project "Newtonsoft.Json"  How? Project -> Manage NuGet Packages -> Newtonsoft.Json
+                ////Program.Root x = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson);
+                var deserializeJson = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+
+
+                if (deserializeJson != null)
+                {
+                    //Variables needed to define the number of pages of patient data and the number of entries.
+                    //Assigning the appropriate data to the variable in the next stage of the code
+                    totalPages = deserializeJson.total_pages;
+                    totalEntries = deserializeJson.total;
+
+                    //DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                    //dateTime = dateTime.AddSeconds(Math.Round(1568550058964 / 1000d)).ToLocalTime();
+                    //Console.WriteLine(dateTime);
+                    List<long> listTimeStamp = new List<long>();
+                    do
                     {
-                        foreach (var item in deserializeJson.data)
+                        if (actualPageUser > 1)
                         {
-                            listTimeStamp.Add((long)item.timestamp);
-                            Console.WriteLine(item.meta.weight);
-                            Console.WriteLine(item.meta.height);
-                            Console.WriteLine(item.meta.bmi);
-                            RetrievedDataFromDeserialization tempValueClass = new RetrievedDataFromDeserialization { bodyTemperature = item.vitals.bodyTemperature };
-                            sss.Add(tempValueClass);
+                            ////Select the new HttpClient method or the old WebClient method
+                            //stringWebsiteJson = new WebClient().DownloadString("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
+                            stringWebsiteJson = await Client.GetStringAsync("https://jsonmock.hackerrank.com/api/medical_records?userId=" + idUser + "&page=" + actualPageUser + "\t\t");
+
+                            deserializeJson = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(stringWebsiteJson, new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
                         }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    actualPageUser++;
-                } while (actualPageUser <= totalPages);
-                Console.WriteLine(UnixTimeStampToDateTime(listTimeStamp.Max()));
-                Console.WriteLine(UnixTimeStampToDateTime(listTimeStamp.Min()));
-                
-            }
-            foreach (var item in sss)
-            {         
-                Console.WriteLine(item.bodyTemperature);       
-            }
+                        //else if (actualPageUser == 1)
+                        //{
+                        //    Console.WriteLine(deserializeJson.data[0].timestamp);
+                        //}
+                        //if (actualPageUser == totalPages)
+                        //{
+                        //    Console.WriteLine(deserializeJson.data[deserializeJson.data.Count()-1].timestamp);
+                        //}
+
+                        if (deserializeJson != null)
+                        {
+                            foreach (var item in deserializeJson.data)
+                            {
+                                RetrievedDataFromDeserialization tempValueClass = new RetrievedDataFromDeserialization
+                                {
+                                    bodyTemperature = item.vitals.bodyTemperature,
+                                    bmi = item.meta.bmi,
+                                    DateExamination = UnixTimeStampToDateTime((long)item.timestamp),
+                                    height = item.meta.height,
+                                    weight = item.meta.weight
+                                };
+                                listRetrievedDataFromDeseralization.Add(tempValueClass);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        actualPageUser++;
+                    } while (actualPageUser <= totalPages);
+
+                }
+                List<RetrievedDataFromDeserialization> SortedList = listRetrievedDataFromDeseralization.OrderBy(s => s.DateExamination).ToList();
+
+                Console.WriteLine("-----------------------------------------------------------------------------------\n" +
+                                  "------------------Welcome to the hospital register \"Quick help\"--------------------\n" +
+                                  "-----------------------------------------------------------------------------------\n");
+                Console.WriteLine("Patient ID: " + idUser + "\n" +
+                                  "Total number of entries: " + totalEntries + "\n" +
+                                  "Date of first examination: " + SortedList.Min(s => s.DateExamination) + "\n" +
+                                  "Date of last examination: " + SortedList.Max(s => s.DateExamination) + "\n \n" +
+                                  "Patient condition: \n" +
+                                  "Current growth: " + SortedList[totalEntries - 1].height + "\n" +
+                                  "Current weight: " + SortedList[totalEntries - 1].weight + "\n" +
+                                  "Previous weight: " + SortedList[totalEntries - 2].weight + "\n" +
+                                  "Current body temperature: " + SortedList[totalEntries - 1].bodyTemperature + "\n" +
+                                  "Previous body temperature: " + SortedList[totalEntries - 2].bodyTemperature + "\n\n" +
+                                  "Weight difference: " + (SortedList[totalEntries - 1].weight - SortedList[totalEntries - 2].weight) + "\n" +
+                                  "Body temperature difference: " + (SortedList[totalEntries - 1].bodyTemperature - SortedList[totalEntries - 2].bodyTemperature) + "\n" +
+                                  "Current BMI: " + SortedList[totalEntries - 1].bmi + "\n\n\n" +
+                                  "Recommendations :");
+                var tempHowManyRecommendations = 0;
+                var tempBmi = SortedList[totalEntries - 1].bmi;
+                var tempTemperatureBody = SortedList[totalEntries - 1].bodyTemperature;
+                List<string> listRecommendations = new List<string>();
+
+                if (tempBmi < (decimal)18.5)
+                {
+                    listRecommendations.Add(" - He was found to be underweight. Please increase the weight.");
+                }
+                else if (tempBmi <= (decimal)24.9)
+                {
+                    listRecommendations.Add(" - Normal weight was found. Please maintain your weight.");
+                }
+                else
+                {
+                    listRecommendations.Add(" - Overweight was diagnosed. Please reduce your weight.");
+                }
+                if (tempTemperatureBody < (decimal)36.5)
+                {
+                    listRecommendations.Add(" - Reduced body temperature was found. The patient's temperature should be increased.");
+                }
+                else if (tempTemperatureBody <= (decimal)37)
+                {
+                    listRecommendations.Add(" - Correct body temperature was found. The temperature must be maintained.");
+                }
+                else
+                {
+                    listRecommendations.Add(" - High body temperature was detected. The patient's body temperature should be reduced.");
+                }
+
+                foreach (var item in listRecommendations)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("\n\n\n\n To search for another patient, press \"enter\"");
+            } while (Console.ReadKey().Key == ConsoleKey.Enter);
         }
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
@@ -115,12 +176,16 @@ namespace Simple_Example_JSON_In_C__With_REST_API_Call
             return dateTime;
         }
         //List with deserialized data
-        public static List<RetrievedDataFromDeserialization> sss = new List<RetrievedDataFromDeserialization>();
+        public static List<RetrievedDataFromDeserialization> listRetrievedDataFromDeseralization = new List<RetrievedDataFromDeserialization>();
 
         //Class in list with data deserialization
         public class RetrievedDataFromDeserialization
         {
-            public double bodyTemperature { get; set; }
+            public decimal? bmi { get; set; }
+            public decimal bodyTemperature { get; set; }
+            public DateTime DateExamination { get; set; }
+            public int height { get; set; }
+            public int weight { get; set; }
         }
 
 
@@ -173,11 +238,16 @@ namespace Simple_Example_JSON_In_C__With_REST_API_Call
 
         public class Vitals
         {
+            private decimal _bodyTemperature;
             public int bloodPressureDiastole { get; set; }
             public int bloodPressureSystole { get; set; }
             public int pulse { get; set; }
             public int breathingRate { get; set; }
-            public double bodyTemperature { get; set; }
+            public decimal bodyTemperature
+            {
+                get { return _bodyTemperature; }
+                set { _bodyTemperature = Math.Round(((value - 32) / (decimal)1.8),1); }
+            }
         }
     }
 }
